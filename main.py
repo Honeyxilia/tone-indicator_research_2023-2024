6609e9bdcbe689672f2a8e880d9f1053b63b9529
Lena MONTENOT <lena.montenot@gmail.com>
2024-04-15 22:11:21 +0200

    added topic detection

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import ti_values
 
 def get_ti_info_about_user(username, headers, params):
     ti_count = {
+        "Total number of topics": 0,
+        "Total number of topics w/ tone indicators": 0,
         "Total number of comments": 0,
         "Total number of comments w/ tone indicators": 0,
         "Total number of tone indicators": 0,
@@ -28,6 +30,69 @@ def get_ti_info_about_user(username, headers, params):
         ],
     ]
 
+    # Topics
+    while True:
+        request_resp = requests.get(
+            f"https://oauth.reddit.com/user/{username}/submitted",
+            headers=headers,
+            params=params,
+            timeout=10
+        )
+        request_resp.raise_for_status()
+        json_sujets = request_resp.json()
+
+        ti_count["Total number of topics"] += json_sujets['data']['dist']
+        next_page = json_sujets['data']['after']
+
+        for sujet in json_sujets['data']['children']:
+            ti_found = re.findall(
+                match_ti,
+                sujet['data']['title']
+            )
+
+            if ti_found:
+                ti_count["Total number of topics w/ tone indicators"] += 1
+                for ti in ti_found:
+                    ti_count["Total number of tone indicators"] += 1
+                    ti_count[ti[0]] += 1
+
+                    output_sheet.append(
+                        [
+                            ti[0],
+                            sujet['data']['created'],
+                            sujet['data']['subreddit_name_prefixed'],
+                            "(in title)",
+                            f"https://www.reddit.com{sujet['data']['permalink']}",
+                        ]
+                    )
+
+            ti_found = re.findall(
+                match_ti,
+                sujet['data']["selftext"]
+            )
+
+            if ti_found:
+                ti_count["Total number of topics w/ tone indicators"] += 1
+                for ti in ti_found:
+                    ti_count["Total number of tone indicators"] += 1
+                    ti_count[ti[0]] += 1
+
+                    output_sheet.append(
+                        [
+                            ti[0],
+                            sujet['data']['created'],
+                            sujet['data']['subreddit_name_prefixed'],
+                            sujet['data']['selftext'],
+                            f"https://www.reddit.com{sujet['data']['permalink']}",
+                        ]
+                    )
+
+        if next_page is None:
+            params.pop('after')
+            break
+        params['after'] = next_page
+
+    # Comments
     while True:
         request_resp = requests.get(
             f"https://oauth.reddit.com/user/{username}/comments",
@@ -58,16 +123,15 @@ def get_ti_info_about_user(username, headers, params):
                             commentaire['data']['created_utc'],
                             commentaire['data']['subreddit_name_prefixed'],
                             commentaire['data']['body'],
-                            commentaire['data']['link_permalink'],
+                            commentaire['data']['permalink'],
                         ]
                     )
 
         if next_page is None:
-
+            params.pop('after')
             break
         params['after'] = next_page
 
-    params.pop('after')
     output_ti = [username]
     output_ti += list(ti_count.values())
 
@@ -104,6 +168,8 @@ def main():
             [
                 [
                     "Username",
+                    "Total number of scouted topics",
+                    "Numbers of topics w/ tone indicators",
                     "Total number of scouted comments",
                     "Number of comments w/ tone indicators",
                     "Number of tone indicators",
@@ -137,11 +203,13 @@ def main():
         output_sheets[username] = output_sheet
 
         print(f"Searched tone indicators : {' '.join(ti_values.ti_count.keys())}")
-        print(f"Total number of comments : {output_ti[1]}")
-        print(f"Total number of comments w/ tone indicators: {output_ti[2]}")
-        print(f"Total number of tone indicators: {output_ti[3]}")
+        print(f"Total number of topics : {output_ti[1]}")
+        print(f"Total number of topics w/ tone indicators: {output_ti[2]}")
+        print(f"Total number of comments : {output_ti[3]}")
+        print(f"Total number of comments w/ tone indicators: {output_ti[4]}")
+        print(f"Total number of tone indicators: {output_ti[5]}")
 
-        for i in range(4, len(output_ti)):
+        for i in range(6, len(output_ti)):
             print(f"Comments with {output_sheets['General statistics'][0][i]} : {output_sheets['General statistics'][1][i]}")
 
     pyexcel.Book(output_sheets).save_as("ti_stats.xlsx")
